cc028a451267c72a544605333a232911811fe0ce
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-05-06 21:25:22 +0530

    Update day_8.py

--- day_8.py
+++ day_8.py
@@ -47,6 +47,16 @@ def prime_checker(number):
     print("It's not a prime number.")
   else:
     print("It's a prime number.")
+
+# Can also be written as:
+#   is_prime = True
+#   for i in range(2, number):
+#     if number % i == 0:
+#       is_prime = False
+#   if is_prime:
+#     print("It's a prime number.")
+#   else:
+#     print("It's not a prime number.")
    
 # Write your code above this line 👆
     
