7fd53e7241a95a1018423d4272f880a56032460e
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-05-07 14:52:29 +0530

    Update day_8_CC.py

--- day_8_CC.py
+++ day_8_CC.py
@@ -66,7 +66,7 @@ while acc_input is True:
         print("Bye.")
         acc_input = False
 
-
+# =================================================================================================================
 # alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 # def caesar(start_text, shift_amount, cipher_direction):
@@ -97,3 +97,43 @@ while acc_input is True:
 #   if restart == "no":
 #     should_end = True
 #     print("Goodbye")
+
+# =========================================================================================================================
+# My Updated code.
+# # from day_8_art import logo
+
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# # print(logo)
+
+
+# def caesar(start_text, shift_amount, cipher_direction):
+#     end_text = ""
+#     if cipher_direction == "decode":
+#         shift_amount *= -1
+#     for char in start_text:
+#         if char in alphabet:
+#             position = alphabet.index(char)
+#             new_position = position + shift_amount
+#             # print(new_position)
+#             if new_position > len(alphabet):
+#                 new_shift_index = new_position % len(alphabet)
+#                 end_text += alphabet[new_shift_index]
+#             else:
+#                 end_text += alphabet[new_position]
+#         else:
+#             end_text += char
+#     print(f"Here's the {cipher_direction}d result: {end_text}")
+    
+
+# acc_input = True
+# while acc_input is True:
+#     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+#     text = input("Type your message:\n").lower()
+#     shift = int(input("Type the shift number:\n"))
+#     shift = shift % 26
+#     caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+#     user_input = input("Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
+#     if user_input == 'no':
+#         print("Bye.")
+#         acc_input = False
