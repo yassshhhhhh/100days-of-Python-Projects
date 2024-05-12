0233e6486cb8fecbfb1d9175d691be6b3a8e86b6
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-05-12 14:59:36 +0530

    Update day_12P.py

--- day_12P.py
+++ day_12P.py
@@ -8,8 +8,10 @@ logo = """
 print(logo)
 print("Welcome to Guess the number!")
 print("I'm thinking of a number between 1 and 100.")
-number = random.randrange(100)  
+number = random.randrange(1, 100)  
 print("Number:", number)
+EASY_LEVEL = 10
+HARD_LEVEL = 5
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
 
 def game(attemps):
@@ -17,18 +19,19 @@ def game(attemps):
         print(f"You have {attemps} attemps remaining to guess the number.")
         guess = int(input("Make a guess: "))
         if guess == number:
-            return "You guessed it right!."
+            return f"You guessed it right!, the number was {number}."
         elif guess < number:
-            print("Too low")
+            print("Too low \nGuess again")
+
             attemps-= 1
         elif guess > number:
-            print("Too high")
+            print("Too high \nGuess again")
             attemps -= 1
-    return "You've run out of guesses, you lose."
+    return "Ohh noo! You've run out of guesses, you lose."
 
 if difficulty == "hard":
-    print(game(5))
+    print(game(HARD_LEVEL))
 elif difficulty == "easy":
-    print(game(10))
+    print(game(EASY_LEVEL))
 else:
     print("Invalid input.")
