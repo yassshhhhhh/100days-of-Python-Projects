7c2854cd3de325fb5f68c9493e505903b0b8c99b
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-05-17 12:34:35 +0530

    Higher or lower

--- day_14.py
+++ day_14.py
@@ -4,13 +4,10 @@ import random
 def game(question_A, question_B):
     print(f"Compare A: {question_A['name']}, a {question_A['description']}, from {question_A['country']}")
     A = question_A['follower_count']
-    print(""" 
-        __   __   ___   
-        \ \ / /  / __|  
-        \ V /   \__ \  
-        _\_/_   |___/  
-        _| """"|_|"""""| 
-        "`-0-0-'"`-0-0-' 
+    print("""   
+         _  _____
+        | |/ (_-<
+        |___/___/
     """)
     print(f"Against B: {question_B['name']}, a {question_B['description']}, from {question_B['country']}")
     B = question_B['follower_count']
@@ -21,43 +18,44 @@ def game(question_A, question_B):
         act_ans = B
         return act_ans
 
-# def compare(act_ans):
-#     if act_ans == foll_count:
-#         score += 1
-#         print(f"You're right! Current score: {score}.")
-#         return 1
-#     else:
-#         print(f"Sorry, that's wrong. Final score: {score}.")
-#         return 0
-    
-    
-# def compare(Ques_a, Ques_b):
-#     if Ques_a['follower_count'] > Ques_b['follower_count']:
-#         game()
 score = 0
 game_is = True
-while game_is is True:
-    question_A = random.choice(data)
+print(""" 
+   __ ___      __          
+  / // (_)__ _/ /  ___ ____
+ / _  / / _ `/ _ \/ -_) __/
+/_//_/_/\_, /_//_/\__/_/   
+     __/___/__             
+    / _ \/ __/             
+   _\___/_/                
+  / /__ _    _____ ____    
+ / / _ \ |/|/ / -_) __/    
+/_/\___/__,__/\__/_/                                                                                                                                                               
+""")
+question_A = random.choice(data)
+question_B = random.choice(data)
+if question_A == question_B:
     question_B = random.choice(data)
-    # game(question_A, question_B)
-    if question_A != question_B:
-        act_ans = game(question_A, question_B)
-        print(act_ans)
-        ans = input("Who has more followers? Type 'A' or 'B': ").lower()
-        if ans == "a":
-            foll_count = question_A['follower_count']
-            print(foll_count)
-        elif ans == "b":
-            foll_count = question_B['follower_count']
-            print(foll_count)
-        # final = compare(act_ans)
-        if act_ans == foll_count:
-            score += 1
-            print(f"You're right! Current score: {score}.")
-            final = 1
-        else:
-            print(f"Sorry, that's wrong. Final score: {score}.")
-            final = 0
-
-        if final == 0:
-            game_is = False
+while game_is is True:
+    act_ans = game(question_A, question_B)
+    # print(act_ans)
+    ans = input("Who has more followers? Type 'A' or 'B': ").lower()
+    if ans == "a":
+        foll_count = question_A['follower_count']
+        # print(foll_count)
+    elif ans == "b":
+        foll_count = question_B['follower_count']
+        # print(foll_count)
+    # clear()
+    # print(logo)
+    if act_ans == foll_count:
+        score += 1
+        print(f"You're right! Current score: {score}.")
+        final = 1
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}.")
+        final = 0
+    question_A = question_B
+    question_B = random.choice(data)
+    if final == 0:
+        game_is = False
