e55efb18ff14e19cdbc6bfb75c1715dafdcc28f0
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-05-18 22:04:30 +0530

    Treasure Island

--- day_3P.py
+++ day_3P.py
@@ -30,7 +30,6 @@ print("Your mission is to find the treasure.")
 
 #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
-#Write your code below this line 👇
 entry = input("You are at a crossroad. Where do you want to go? Type 'left' or 'right' ").lower()
 if entry == "left":
   swim = input("You have come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ")
