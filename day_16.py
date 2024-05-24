cab82ded5385dae2fe2602f31e558a7923c6fc21
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-05-24 15:22:28 +0530

    Oops - CM

--- day_16.py
+++ day_16.py
@@ -1,122 +1,129 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
-
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+# class MenuItem:
+#     """Models each Menu Item."""
+#     def __init__(self, name, water, milk, coffee, cost):
+#         self.name = name
+#         self.cost = cost
+#         self.ingredients = {
+#             "water": water,
+#             "milk": milk,
+#             "coffee": coffee
+#         }
 
 
-# def report(quantities):
-#     print(f'Water: {quantities["water"]}ml \nMilk: {quantities["milk"]}ml \nCoffee: {quantities["coffee"]}g \nMoney: ${money}')
+# class Menu:
+#     """Models the Menu with drinks."""
+#     def __init__(self):
+#         self.menu = [
+#             MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+#             MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
+#             MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3),
+#         ]
 
+#     def get_items(self):
+#         """Returns all the names of the available menu items"""
+#         options = ""
+#         for item in self.menu:
+#             options += f"{item.name}/"
+#         return options
 
-# def insert_coins():
-#     quarters = float(input("How many quarters?: "))
-#     dimes = float(input("How many dimes?: "))
-#     nickles = float(input("How many nickles?: "))
-#     cents = float(input("How many cents?: "))
-#     total_cost = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (cents * 0.01)
-#     return total_cost
+#     def find_drink(self, order_name):
+#         """Searches the menu for a particular drink by name. Returns that item if it exists, otherwise returns None"""
+#         for item in self.menu:
+#             if item.name == order_name:
+#                 return item
+#         print("Sorry that item is not available.")
 
+# class MoneyMachine:
 
-# money = 0
-# game = True
-# while game:
-#     user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+#     CURRENCY = "$"
 
-#     if user_input == "report":
-#         report(resources)
-#     elif user_input == "espresso":
-#         if resources["water"] < MENU["espresso"]["ingredients"]["water"]:
-#             print("Sorry there is not enough water.")
-#         elif resources["coffee"] < MENU["espresso"]["ingredients"]["coffee"]:
-#             print("Sorry there is not enough coffee.")
+#     COIN_VALUES = {
+#         "quarters": 0.25,
+#         "dimes": 0.10,
+#         "nickles": 0.05,
+#         "pennies": 0.01
+#     }
+
+#     def __init__(self):
+#         self.profit = 0
+#         self.money_received = 0
+
+#     def report(self):
+#         """Prints the current profit"""
+#         print(f"Money: {self.CURRENCY}{self.profit}")
+
+#     def process_coins(self):
+#         """Returns the total calculated from coins inserted."""
+#         print("Please insert coins.")
+#         for coin in self.COIN_VALUES:
+#             self.money_received += int(input(f"How many {coin}?: ")) * self.COIN_VALUES[coin]
+#         return self.money_received
+
+#     def make_payment(self, cost):
+#         """Returns True when payment is accepted, or False if insufficient."""
+#         self.process_coins()
+#         if self.money_received >= cost:
+#             change = round(self.money_received - cost, 2)
+#             print(f"Here is {self.CURRENCY}{change} in change.")
+#             self.profit += cost
+#             self.money_received = 0
+#             return True
 #         else:
-#             cost = float(MENU["espresso"]["cost"])
-#             print(f"Espresso costs ${cost}, Please insert coins.")
-#             coins = insert_coins()
-#             if coins < cost:
-#                 print("Sorry that's not enough money. Money refunded.")
-#             # elif insert_coins() == cost:
-#             #     print("Here is your espresso ☕. Enjoy!")
-#             else:
-#                 change = round(coins - cost, 2)
-#                 print(f"Here is ${change} in change.")
-#                 print("Here is your espresso ☕. Enjoy!")
-#                 resources["water"] = resources["water"] - MENU["espresso"]["ingredients"]["water"]
-#                 # resources["milk"] = resources["milk"] - 0
-#                 resources["coffee"] = resources["coffee"] - MENU["espresso"]["ingredients"]["coffee"]
-#                 money += cost
-#     elif user_input == "latte":
-#         if resources["water"] < MENU["latte"]["ingredients"]["water"]:
-#             print("Sorry there is not enough water.")
-#         elif resources["milk"] < MENU["latte"]["ingredients"]["milk"]:
-#             print("Sorry there is not enough milk.")
-#         elif resources["coffee"] < MENU["latte"]["ingredients"]["coffee"]:
-#             print("Sorry there is not enough coffee.")
-#         else:
-#             cost = float(MENU["latte"]["cost"])
-#             print(f"Latte costs ${cost}, Please insert coins.")
-#             coins = insert_coins()
-#             change = round(coins - cost, 2)
-#             if coins < cost:
-#                 print("Sorry that's not enough money. Money refunded.")
-#             else:
-#                 print(f"Here is ${change} in change.")
-#                 print("Here is your latte ☕. Enjoy!")
-#                 resources["water"] = resources["water"] - MENU["latte"]["ingredients"]["water"]
-#                 resources["milk"] = resources["milk"] - MENU["latte"]["ingredients"]["milk"]
-#                 resources["coffee"] = resources["coffee"] - MENU["latte"]["ingredients"]["coffee"]
-#                 money += cost
-#     elif user_input == "cappuccino":
-#         if resources["water"] < MENU["cappuccino"]["ingredients"]["water"]:
-#             print("Sorry there is not enough water.")
-#         elif resources["milk"] < MENU["cappuccino"]["ingredients"]["milk"]:
-#             print("Sorry there is not enough milk.")
-#         elif resources["coffee"] < MENU["cappuccino"]["ingredients"]["coffee"]:
-#             print("Sorry there is not enough coffee.")
-#         else:
-#             cost = float(MENU["cappuccino"]["cost"])
-#             print(f"Cappuccino costs ${cost}, Please insert coins.")
-#             coins = insert_coins()
-#             change = round(coins - cost, 2)
-#             if coins < cost:
-#                 print("Sorry that's not enough money. Money refunded.")
-#             else:
-#                 print(f"Here is ${change} in change.")
-#                 print("Here is your cappuccino ☕. Enjoy!")
-#                 resources["water"] = resources["water"] - MENU["cappuccino"]["ingredients"]["water"]
-#                 resources["milk"] = resources["milk"] - MENU["cappuccino"]["ingredients"]["milk"]
-#                 resources["coffee"] = resources["coffee"] - MENU["cappuccino"]["ingredients"]["coffee"]
-#                 money += cost
-#     elif user_input == "off":
-#         game = False
-# Update above project - Coffee machine with OOPs.
+#             print("Sorry that's not enough money. Money refunded.")
+#             self.money_received = 0
+#             return False
 
-print(MENU)
-print(resources)
+# class CoffeeMaker:
+#     """Models the machine that makes the coffee"""
+#     def __init__(self):
+#         self.resources = {
+#             "water": 300,
+#             "milk": 200,
+#             "coffee": 100,
+#         }
+
+#     def report(self):
+#         """Prints a report of all resources."""
+#         print(f"Water: {self.resources['water']}ml")
+#         print(f"Milk: {self.resources['milk']}ml")
+#         print(f"Coffee: {self.resources['coffee']}g")
+
+#     def is_resource_sufficient(self, drink):
+#         """Returns True when order can be made, False if ingredients are insufficient."""
+#         can_make = True
+#         for item in drink.ingredients:
+#             if drink.ingredients[item] > self.resources[item]:
+#                 print(f"Sorry there is not enough {item}.")
+#                 can_make = False
+#         return can_make
+
+#     def make_coffee(self, order):
+#         """Deducts the required ingredients from the resources."""
+#         for item in order.ingredients:
+#             self.resources[item] -= order.ingredients[item]
+#         print(f"Here is your {order.name} ☕️. Enjoy!")
+
+
+
+# from menu import Menu, MenuItem
+# from coffee_maker import CoffeeMaker
+# from money_machine import MoneyMachine
+
+
+# menu = Menu()
+# coffee_maker = CoffeeMaker()
+# money_machine = MoneyMachine()
+
+# is_game = True
+# while is_game:
+#     options = menu.get_items()
+#     choice = input(f"What do you like? ({options}): ").lower()
+#     if choice == "off":
+#         is_game = False
+#     elif choice == "report":
+#         coffee_maker.report()
+#         money_machine.report()
+#     else:
+#         drink = menu.find_drink(choice)
+#         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+#             coffee_maker.make_coffee(drink)
