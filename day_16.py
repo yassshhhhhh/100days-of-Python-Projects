5c3bde27ed92bbd8b3ba61d549f0a7d2c87efda1
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-05-24 15:34:49 +0530

    Oops - CM

--- day_16.py
+++ day_16.py
@@ -125,5 +125,6 @@
 #         money_machine.report()
 #     else:
 #         drink = menu.find_drink(choice)
+#         print(f"{choice} costs ${drink.cost}.")
 #         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
 #             coffee_maker.make_coffee(drink)
