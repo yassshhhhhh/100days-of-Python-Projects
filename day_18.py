9aa0eebbfa3be6ffd4dacfbe69e3ea1ce07015d4
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-05-26 23:37:15 +0530

    Turtle

--- day_18.py
+++ day_18.py
@@ -1,1 +1,17 @@
-# Turtle Model
+from turtle import Turtle, Screen
+
+meow = Turtle()
+meow.shape("turtle")
+meow.color("cyan", "navy")
+meow.speed("fast")
+for _ in range(4):
+    meow.forward(120)
+    meow.right(90)
+
+for _ in range(60):
+    meow.forward(100)
+    meow.left(122)
+
+
+meow_screen = Screen()
+meow_screen.exitonclick()
