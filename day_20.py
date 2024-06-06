29653e85e79d064a09f47b0bfd9276ce5650555a
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-06-06 14:32:11 +0530

    Snake game

---
+++ day_20.py
@@ -0,0 +1,73 @@
+from turtle import Screen
+import time
+# from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+RIGHT = 0
+DOWN = 270
+LEFT = 180
+
+
+class Snake:
+
+    def __init__(self):
+        self.snake_body = []
+        self.create_snake()
+        self.head = self.snake_body[0]
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            meow = Turtle('square')
+            meow.color("white")
+            meow.penup()
+            meow.goto(position)
+            self.snake_body.append(meow)
+
+    def move(self):
+        for body in range(len(self.snake_body) - 1, 0, -1):
+            new_x = self.snake_body[body-1].xcor()
+            new_y = self.snake_body[body-1].ycor()
+            self.snake_body[body].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+
+screen.exitonclick()
