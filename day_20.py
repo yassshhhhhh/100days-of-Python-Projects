33f6d6842f3819ae962d598bdb742e2dc59345b9
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-06-09 22:31:26 +0530

    Snake game

--- day_20.py
+++ day_20.py
@@ -1,5 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time
+# from snake import Snake
+
+from turtle import Turtle
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -7,7 +10,6 @@ RIGHT = 0
 DOWN = 270
 LEFT = 180
 
-# from snake import Snake
 
 class Snake:
 
@@ -18,11 +20,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            meow = Turtle('square')
-            meow.color("white")
-            meow.penup()
-            meow.goto(position)
-            self.snake_body.append(meow)
+            self.add_snake_body(position)
+
+    def add_snake_body(self, position):
+        meow = Turtle('square')
+        meow.color("white")
+        meow.penup()
+        meow.goto(position)
+        self.snake_body.append(meow)
+
+    def extend(self):
+        self.add_snake_body(self.snake_body[-1].position())
 
     def move(self):
         for body in range(len(self.snake_body) - 1, 0, -1):
@@ -48,6 +56,61 @@ class Snake:
             self.head.setheading(LEFT)
 
 
+# from food import Food
+
+import random
+from turtle import Turtle
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        ran_x_axis = random.randint(-270, 270)
+        ran_y_axis = random.randint(-270, 270)
+        self.goto(ran_x_axis, ran_y_axis)
+
+
+# from scoreboard import Scoreboard
+
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT_1 = ("courier", 24, "normal")
+FONT_2 = ("courier", 30, "normal")
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=FONT_1)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg="Game Over", align=ALIGNMENT, font=FONT_2)
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
+
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -56,12 +119,14 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(snake.up, "Up")
-screen.onkey(snake.right, "Right")
-screen.onkey(snake.down, "Down")
-screen.onkey(snake.left, "Left")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.right, "d")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
 
 is_game_on = True
 while is_game_on:
@@ -69,7 +134,23 @@ while is_game_on:
     time.sleep(0.1)
     snake.move()
 
+    # detecting the food collision
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
-# This is the last time I'm doing single commits. 
+    # detecting the wall collision.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        is_game_on = False
+        scoreboard.game_over()
+
+    # detecting collision with tail.
+    for segment in snake.snake_body:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            is_game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
