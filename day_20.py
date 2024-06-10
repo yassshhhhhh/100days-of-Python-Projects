df45f3610913cbfc3932d5a33fb32e75f96acb2c
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-06-10 10:58:12 +0530

    Snake Game

--- day_20.py
+++ day_20.py
@@ -146,11 +146,10 @@ while is_game_on:
         scoreboard.game_over()
 
     # detecting collision with tail.
-    for segment in snake.snake_body:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.snake_body[1:]:
+        if snake.head.distance(segment) < 10:
             is_game_on = False
             scoreboard.game_over()
+
 
 screen.exitonclick()
