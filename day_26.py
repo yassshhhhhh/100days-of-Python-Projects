1aa624967babcb0423b30dcff7179f32a1f43f8b
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-06-13 09:16:20 +0530

    NATO Alphabet

--- day_26.py
+++ day_26.py
@@ -21,3 +21,18 @@
 #     # print(row.student)
 #     if row.score > 97:
 #         print(f"{row.student} is the topper with a score of {row.score}.")
+
+#TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+# print(type(data))
+dict_data = {row.letter: row.code for (index, row) in data.iterrows()}
+print(dict_data)
+
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+user_input = input("Enter you name: ").upper()
+letters_list = [dict_data[letter] for letter in user_input]
+print(letters_list)
