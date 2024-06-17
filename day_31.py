8424723f2c708b7b445f51b555e5ab44fe828842
Yashraj <104720443+yassshhhhhh@users.noreply.github.com>
2024-06-17 22:22:16 +0530

    Flash cards

---
+++ day_31.py
@@ -0,0 +1,10 @@
+# Flash Cards - To learn languages efficiently.
+
+import tkinter
+
+window = tkinter.Tk()
+window.title("Flash Cards")
+window.config(width=600, height=600, bg="red")
+
+window.mainloop()
+
